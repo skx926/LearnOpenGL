041fa22704729f495939096a44bf4a11b3b9acd8
孙凯旋 <skx926@gmail.com>
2018-09-06 11:22:09 +0800

    Draw triangle

--- glfw/glfw/main.cpp
+++ glfw/glfw/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void draw();
 
 int main(int argc, const char * argv[]) {
     
@@ -47,6 +48,8 @@ int main(int argc, const char * argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        draw();
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -64,4 +67,106 @@ void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+void draw() {
+//    float vertices[] = {
+//        -0.5f, -0.5f, 0.0f,
+//         0.5f, -0.5f, 0.0f,
+//         0.0f,  0.5f, 0.0f
+//    };
+    
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+    
+    unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    
+    const char *vertextShaderSource = "\
+    #version 330 core\
+    layout (location = 0) in vec3 aPos;\
+    \
+    void main()\
+    {\
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
+    }";
+    
+    unsigned int vertextShader;
+    vertextShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertextShader, 1, &vertextShaderSource, NULL);
+    glCompileShader(vertextShader);
+    
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertextShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertextShader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+        return;
+    }
+    
+    const char *fragmentShaderSource = "\
+    #version 330 core\
+    out vec4 FragColor;\
+    \
+    void main()\
+    {\
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\
+    }";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+    }
+    
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertextShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+    }
+    glDeleteShader(vertextShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glDrawArrays(GL_TRIANGLES, 0 , 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+    
 }
